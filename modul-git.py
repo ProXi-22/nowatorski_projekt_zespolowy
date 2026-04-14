2d0563c4e8340d41a236f3c3dad4182905ca3366
ProXi22 <michalopl15@gmail.com>
2026-04-14 22:13:16 +0200

    dodana nowa metoda pobierz_repo

--- modul-git.py
+++ modul-git.py
@@ -1,3 +1,12 @@
-from git import repo, Repo
+import os
+import tempfile
+from git import Repo
 
-
+def pobierz_repo(zrodlo):
+    if zrodlo.startswith("http"):
+        folder_tymczasowy = tempfile.mkdtemp()
+        print(f"Klonowanie repozytorium z: {zrodlo}")
+        repo = Repo.clone_from(zrodlo, folder_tymczasowy)
+        return repo, folder_tymczasowy
+    else:
+        return Repo(zrodlo), zrodlo
