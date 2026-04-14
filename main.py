7b417b8e49ec44dbc178d272c1a5a497890e7511
ProXi22 <michalopl15@gmail.com>
2026-04-14 23:15:43 +0200

    dodanie argumentow dla CLI

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import argparse
+from platform import release
+
 from modul_git import przygotuj_liste_zmian
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--repo", required=True)
+parser.add_argument("--od", required=True)
+parser.add_argument("--do", required=True)
+argumenty = parser.parse_args()
+
+lista_zmian = przygotuj_liste_zmian(argumenty.repo, argumenty.od, argumenty.do)
+
+if not lista_zmian:
+    print("Nie znaleziono commitow w podanym zakresie.")
+    exit()
+
+print(f"Znaleziono {len(lista_zmian)} commitow.\n")
+print("Generowanie release notes...")
 
