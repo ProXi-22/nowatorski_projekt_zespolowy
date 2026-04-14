cb40d02e7b560b4f452fe3486c568f2dc057b104
ProXi22 <michalopl15@gmail.com>
2026-04-14 22:36:56 +0200

    dodana nowa metoda pobierz_diff oraz przygotuj_liste_zmian

--- modul-git.py
+++ modul-git.py
@@ -10,3 +10,27 @@ def pobierz_repo(zrodlo):
         return repo, folder_tymczasowy
     else:
         return Repo(zrodlo), zrodlo
+
+def pobierz_diff(commit):
+    if not commit.parents:
+        return ""
+    roznice = commit.diff(commit.parents[0], create_patch=True)
+    tekst_diffa = ""
+    for r in roznice:
+        tekst_diffa += r.diff.decode("utf-8", errors="ignore")
+    return tekst_diffa
+
+def przygotuj_liste_zmian(zrodlo, od_taga, do_taga):
+    repo, _ = pobierz_repo(zrodlo)
+    zakres = f"{od_taga}...{do_taga}"
+    commity = list(repo.iter_commits(zakres))
+    lista_zmian = []
+    for c in commity:
+        zmiana = {
+            "hash": str(c.hexsha)[:7],
+            "wiadomosc": c.message.strip(),
+            "autor": str(c.author),
+            "diff": pobierz_diff(c)
+        }
+        lista_zmian.append(zmiana)
+    return lista_zmian
