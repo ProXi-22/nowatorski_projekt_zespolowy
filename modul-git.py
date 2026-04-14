eb0d2ddd849204f72687f017ce497da4d1aef59f
ProXi22 <michalopl15@gmail.com>
2026-04-14 22:01:00 +0200

    usuniecie metody pobierz_commity

--- modul-git.py
+++ modul-git.py
@@ -1,9 +1,3 @@
 from git import repo, Repo
 
 
-def pobierz_commity(sciezka_repo, od_tag, do_tag):
-    repo = Repo(sciezka_repo)
-    zakres = f"{od_tag}-{do_tag}"
-    commity = list(repo.iter_commits(zakres))
-    return commity
-
